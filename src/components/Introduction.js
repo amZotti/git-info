87607dec18cdd3233078117d9448ddeee61b7a06
Anthony Zotti <Anthonymzotti@gmail.com>
2020-08-13 22:34:14 -0400

    Update starting message

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,10 +3,15 @@ import styled from "styled-components";
 import { Notification } from "./shared.js";
 import { getGithubLink } from "../utils.js";
 
+const Bold = styled.span`
+	font-weight: 700;
+`;
 
 export default () =>(
   <React.Fragment>
-    <Notification>Submit an organization name to proceed</Notification>
-    <p>You could search {getGithubLink("/search","all of Github")} or try an {getGithubLink("/search/advanced","advanced search")}.</p>
+    <Notification>Getting started is easy</Notification>
+    <p>Simply input an <Bold>organization name</Bold> into the menu on the left and <Bold>press enter</Bold></p>
+    <p>For example: <pre>netflix</pre>, <pre>web50</pre>, or <pre>github</pre> are all great places to start</p>
+    <p>You could also search {getGithubLink("/search","all of Github")} or try an {getGithubLink("/search/advanced","advanced search")}.</p>
   </React.Fragment>
 );
